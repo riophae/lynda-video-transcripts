dabb9c728bf193eb086c22c9db2b5fc70526f1ab
riophae <riophaelee@gmail.com>
2017-01-05 04:59:08 +0800

    code cleanup

--- src/test.js
+++ src/test.js
@@ -1,20 +1,5 @@
 /* eslint-disable no-unused-vars */
 
-function testLoop() {
-  const loop = require('./utils/loop');
-
-  let i = 0;
-  loop(10, () => new Promise((res, rej) => {
-    console.log(i);
-    if (i++ < 5) {
-      setTimeout(rej, 1000);
-    } else {
-      res();
-    }
-  }));
-}
-// testLoop();
-
 function testFormatTimestamp() {
   const format = require('./utils/formatTimestamp').default;
 
